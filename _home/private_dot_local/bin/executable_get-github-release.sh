ea1633a1bc786b334439da8a55d4eb8dfb02f9fb
Lockszmith (kateryna) <code@lksz.me>
2024-09-21 15:13:28 -0400

    added update nvim

---
+++ _home/private_dot_local/bin/executable_get-github-release.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+GH_PROJECT="${1}"
+GH_DL_TAG="${2:-latest}"
+GH_FILTER="${3:-deb}"
+if [[ ! "$GH_FILTER" =~ '(' ]]; then
+    GH_FILTER="contains(\"${GH_FILTER}\")"
+fi
+SRC_URL=https://api.github.com/repos/${GH_PROJECT}/releases/${GH_DL_TAG}
+DL_URL=$( \
+    curl -sL curl ${SRC_URL} \
+        | jq -r " \
+            .assets[] \
+            | select(.browser_download_url \
+                | ${GH_FILTER} ) \
+            | .browser_download_url \
+        ")
+[[ -n "$DL_URL" ]] \
+    && printf "%s\n" $DL_URL \
+    || return 1 2>/dev/null || exit 1
