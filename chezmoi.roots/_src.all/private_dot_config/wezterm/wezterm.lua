de47f0c3886aa295fcfadda71be38f9936a2f8de
Lockszmith (Mac@VAST) <code@lksz.me>
2025-04-14 14:05:59 -0400

    Moved chezmoi data one level down under chezmoi.roots

--- chezmoi.roots/_src.all/private_dot_config/wezterm/wezterm.lua
+++ chezmoi.roots/_src.all/private_dot_config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 -- This table will hold the configuration.
@@ -8,20 +8,21 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.window_background_opacity = 0.97
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.9
 config.text_background_opacity = 0.9
 
 -- This is where you actually apply your config choices
 config.audible_bell = "Disabled"
 
 config.visual_bell = {
-  fade_in_function = 'EaseIn',
-  fade_in_duration_ms = 50,
-  fade_out_function = 'Constant',
-  fade_out_duration_ms = 0,
+	fade_in_function = "EaseIn",
+	fade_in_duration_ms = 50,
+	fade_out_function = "Constant",
+	fade_out_duration_ms = 0,
 }
 -- config.colors = {
 --   visual_bell = '#202020',
@@ -33,7 +34,7 @@ config.visual_bell = {
 -- config.color_scheme = 'Apple System Colors'
 -- config.color_scheme = 'Azu (Gogh)'
 -- config.color_scheme = 'Bim (Gogh)'
--- config.color_scheme = 'Cai (Gogh)'
+-- config.colorCMD_scheme = 'Cai (Gogh)'
 -- config.color_scheme = 'CGA'
 -- config.color_scheme = 'Chalkboard'
 -- config.color_scheme = 'Dark Pastel'
@@ -70,7 +71,7 @@ config.visual_bell = {
 -- config.color_scheme = 'niji'
 -- config.color_scheme = 'Oceanic Next (Gogh)'
 -- config.color_scheme = 'Pasque (base16)' -- Purple/Lavendar hues
-config.color_scheme = 'Pencil Dark (Gogh)' -- ****1/2
+config.color_scheme = "Pencil Dark (Gogh)" -- ****1/2
 -- config.color_scheme = 'PencilDark'
 -- config.color_scheme = 'Popping and Locking'
 -- config.color_scheme = 'Rasi (terminal.sexy)'
@@ -89,21 +90,35 @@ config.color_scheme = 'Pencil Dark (Gogh)' -- ****1/2
 -- config.color_scheme = 'Vice Dark (base16)'
 
 -- config.font = wezterm.font 'CaskaydiaCove Nerd Font Mono Regular'
-config.font = wezterm.font_with_fallback {
-    'FiraCode Nerd Font Mono'
-  , 'FiraCode Nerd Font Mono SemBd'
-  , 'FiraCode Nerd Font Mono Ret'
-  , 'FiraMono Nerd Font Mono'
-  , 'DroidSansM Nerd Font'
-  , 'DroidSansMono NF'
-  , 'DroidSansMono'
-  , 'Consolas'
-  , 'Courier New'
-  , 'monospace'
-}
+config.font = wezterm.font_with_fallback({
+	"FiraCode Nerd Font Mono",
+	"FiraCode Nerd Font Mono SemBd",
+	"FiraCode Nerd Font Mono Ret",
+	"FiraMono Nerd Font Mono",
+	"DroidSansM Nerd Font",
+	"DroidSansMono NF",
+	"DroidSansMono",
+	"Consolas",
+	"Courier New",
+	"monospace",
+})
+
+config.disable_default_key_bindings = true
+
 --config.default_prog = { 'pwsh' }
 
--- config.keys = {
+config.keys = {
+	{
+		key = "T",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ShowLauncher,
+	},
+	{
+		key = "T",
+		mods = "SUPER",
+		action = wezterm.action.ShowLauncher,
+	},
+}
 --   {
 --     key = ',',
 --     mods = 'CMD',
@@ -144,71 +159,71 @@ config.font = wezterm.font_with_fallback {
 -- }
 
 config.window_background_gradient = {
-  -- Can be "Vertical" or "Horizontal".  Specifies the direction
-  -- in which the color gradient varies.  The default is "Horizontal",
-  -- with the gradient going from left-to-right.
-  -- Linear and Radial gradients are also supported; see the other
-  -- examples below
-  orientation = 'Vertical',
+	-- Can be "Vertical" or "Horizontal".  Specifies the direction
+	-- in which the color gradient varies.  The default is "Horizontal",
+	-- with the gradient going from left-to-right.
+	-- Linear and Radial gradients are also supported; see the other
+	-- examples below
+	orientation = "Vertical",
 
-  -- Specifies the set of colors that are interpolated in the gradient.
-  -- Accepts CSS style color specs, from named colors, through rgb
-  -- strings and more
-  colors = {
-    '#0f0c29',
-    '#302b63',
-    '#24243e',
-  },
+	-- Specifies the set of colors that are interpolated in the gradient.
+	-- Accepts CSS style color specs, from named colors, through rgb
+	-- strings and more
+	colors = {
+		"#0f0c29",
+		"#302b63",
+		"#24243e",
+	},
 
-  -- Instead of specifying `colors`, you can use one of a number of
-  -- predefined, preset gradients.
-  -- A list of presets is shown in a section below.
-  -- preset = "Warm",
+	-- Instead of specifying `colors`, you can use one of a number of
+	-- predefined, preset gradients.
+	-- A list of presets is shown in a section below.
+	-- preset = "Warm",
 
-  -- Specifies the interpolation style to be used.
-  -- "Linear", "Basis" and "CatmullRom" as supported.
-  -- The default is "Linear".
-  interpolation = 'Linear',
+	-- Specifies the interpolation style to be used.
+	-- "Linear", "Basis" and "CatmullRom" as supported.
+	-- The default is "Linear".
+	interpolation = "Linear",
 
-  -- How the colors are blended in the gradient.
-  -- "Rgb", "LinearRgb", "Hsv" and "Oklab" are supported.
-  -- The default is "Rgb".
-  blend = 'Rgb',
+	-- How the colors are blended in the gradient.
+	-- "Rgb", "LinearRgb", "Hsv" and "Oklab" are supported.
+	-- The default is "Rgb".
+	blend = "Rgb",
 
-  -- To avoid vertical color banding for horizontal gradients, the
-  -- gradient position is randomly shifted by up to the `noise` value
-  -- for each pixel.
-  -- Smaller values, or 0, will make bands more prominent.
-  -- The default value is 64 which gives decent looking results
-  -- on a retina macbook pro display.
-  -- noise = 64,
+	-- To avoid vertical color banding for horizontal gradients, the
+	-- gradient position is randomly shifted by up to the `noise` value
+	-- for each pixel.
+	-- Smaller values, or 0, will make bands more prominent.
+	-- The default value is 64 which gives decent looking results
+	-- on a retina macbook pro display.
+	-- noise = 64,
 
-  -- By default, the gradient smoothly transitions between the colors.
-  -- You can adjust the sharpness by specifying the segment_size and
-  -- segment_smoothness parameters.
-  -- segment_size configures how many segments are present.
-  -- segment_smoothness is how hard the edge is; 0.0 is a hard edge,
-  -- 1.0 is a soft edge.
+	-- By default, the gradient smoothly transitions between the colors.
+	-- You can adjust the sharpness by specifying the segment_size and
+	-- segment_smoothness parameters.
+	-- segment_size configures how many segments are present.
+	-- segment_smoothness is how hard the edge is; 0.0 is a hard edge,
+	-- 1.0 is a soft edge.
 
-  -- segment_size = 11,
-  -- segment_smoothness = 0.0,
+	-- segment_size = 11,
+	-- segment_smoothness = 0.0,
 }
 config.window_background_gradient = null
 config.prefer_to_spawn_tabs = true
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local pane_title = tab.active_pane.title
-  local user_title = tab.active_pane.user_vars.panetitle
+	local pane_title = tab.active_pane.title
+	local user_title = tab.active_pane.user_vars.panetitle
 
-  if user_title ~= nil and #user_title > 0 then
-    pane_title = user_title
-  end
+	if user_title ~= nil and #user_title > 0 then
+		pane_title = user_title
+	end
 
-  return {
-    -- {Background={Color="blue"}},
-    -- {Foreground={Color="white"}},
-    {Text=" " .. pane_title .. " "},
-  }
+	return {
+		-- {Background={Color="blue"}},
+		-- {Foreground={Color="white"}},
+		{ Text = " " .. pane_title .. " " },
+	}
 end)
 
 -- # First and only argument is the desired term title
@@ -243,4 +258,3 @@ end)
 
 -- and finally, return the configuration to wezterm
 return config
-
