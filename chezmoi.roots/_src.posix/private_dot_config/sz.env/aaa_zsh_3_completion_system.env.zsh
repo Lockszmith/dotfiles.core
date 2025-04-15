391fee6cc944aba908a81a4ded22e968309f4f72
Lockszmith (VAST@MacBook) <code@lksz.me>
2025-04-15 12:31:19 -0400

    rename some sz.env files to adjust the load order

--- chezmoi.roots/_src.posix/private_dot_config/sz.env/aaa_zsh_3_completion_system.env.zsh
+++ chezmoi.roots/_src.posix/private_dot_config/sz.env/aaa_zsh_3_completion_system.env.zsh
@@ -1,8 +1,7 @@
 #!/usr/bin/env -S zsh -c 'echo "Not a user script. source(aka .) only"'
 
-BASE_SHELL=${BASE_SHELL:-${SHELL##*/}}
-
-if [[ "${BASE_SHELL}" == "zsh" ]]; then
+zinit load 'marlonrichert/zsh-autocomplete'
+if false; then
     autoload -Uz +X compinit bashcompinit && compinit && bashcompinit
 
     zstyle ':completion:*' menu yes select
