965d9f030fe2cf0c8763db5cc777e9ad57329d3e
Lockszmith (VAST@MacBook) <code@lksz.me>
2025-07-17 10:13:23 -0400

    Update a lot of stuff, bad tracking, but need to update

--- chezmoi.roots/_home.macos/private_dot_config/sz.env/011_zsh_3_completion_system.env.zsh
+++ chezmoi.roots/_home.macos/private_dot_config/sz.env/011_zsh_3_completion_system.env.zsh
@@ -1,1 +1,31 @@
-../../../_home/private_dot_config/sz.env/011_zsh_3_completion_system.env.zsh
+#!/usr/bin/env -S zsh -c 'echo "Not a user script. source(aka .) only"'
+
+if [ -n "$ZI_HOME" ]; then
+    #zinit light 'marlonrichert/zsh-autocomplete'
+    zinit light 'zsh-users/zsh-completions'
+    zinit light 'zsh-users/zsh-autosuggestions'
+else
+    znap source 'zdharma-continuum/fast-syntax-highlighting'
+    #znap source 'marlonrichert/zsh-autocomplete'
+    znap source 'zsh-users/zsh-completions'
+    znap source 'zsh-users/zsh-autosuggestions'
+
+    # Make Tab and ShiftTab cycle completions on the command line
+    # bindkey              '^I'         menu-complete
+    # bindkey "$terminfo[kcbt]" reverse-menu-complete
+
+    # Make Tab and ShiftTab go to the menu
+                                bindkey              '^I' menu-select
+    [ -z "$terminfo[kcbt]" ] || bindkey "$terminfo[kcbt]" menu-select
+
+    # Make Tab and ShiftTab change the selection in the menu
+                                 bindkey -M menuselect              '^I'         menu-complete
+     [ -z "$terminfo[kcbt]" ] || bindkey -M menuselect "$terminfo[kcbt]" reverse-menu-complete
+
+    # Make ← and → always move the cursor on the command line
+    # bindkey -M menuselect  '^[[D' .backward-char  '^[OD' .backward-char
+    # bindkey -M menuselect  '^[[C'  .forward-char  '^[OC'  .forward-char
+fi
+
+# vim: set ft=sh expandtab tabstop=4 shiftwidth=4:
+
--- chezmoi.roots/_home/private_dot_config/sz.env/011_zsh_3_completion_system.env.zsh
+++ chezmoi.roots/_home/private_dot_config/sz.env/011_zsh_3_completion_system.env.zsh
@@ -4,7 +4,7 @@ if [ -n "$ZI_HOME" ]; then
     zinit light 'marlonrichert/zsh-autocomplete'
     zinit light 'zsh-users/zsh-autosuggestions'
 else
-    znap source 'marlonrichert/zsh-autocomplete'
+    #znap source 'marlonrichert/zsh-autocomplete'
     znap source 'zsh-users/zsh-autosuggestions'
 
     # Make Tab and ShiftTab cycle completions on the command line
@@ -12,12 +12,12 @@ else
     # bindkey "$terminfo[kcbt]" reverse-menu-complete
 
     # Make Tab and ShiftTab go to the menu
-    bindkey              '^I' menu-select
-    bindkey "$terminfo[kcbt]" menu-select
+                                bindkey              '^I' menu-select
+    [ -z "$terminfo[kcbt]" ] || bindkey "$terminfo[kcbt]" menu-select
 
     # Make Tab and ShiftTab change the selection in the menu
-    bindkey -M menuselect              '^I'         menu-complete
-    bindkey -M menuselect "$terminfo[kcbt]" reverse-menu-complete
+                                 bindkey -M menuselect              '^I'         menu-complete
+     [ -z "$terminfo[kcbt]" ] || bindkey -M menuselect "$terminfo[kcbt]" reverse-menu-complete
 
     # Make ← and → always move the cursor on the command line
     # bindkey -M menuselect  '^[[D' .backward-char  '^[OD' .backward-char
