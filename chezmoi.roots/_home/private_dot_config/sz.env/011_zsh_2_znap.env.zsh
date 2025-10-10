6e179fc7742c0930b378c290a5dd714e7a92f179
Gal Szkolnik <gal.szkolnik@vastdata.com>
2025-10-10 16:40:32 +0000

    Add znap-cleanup

--- chezmoi.roots/_home/private_dot_config/sz.env/011_zsh_2_znap.env.zsh
+++ chezmoi.roots/_home/private_dot_config/sz.env/011_zsh_2_znap.env.zsh
@@ -2,6 +2,10 @@
 
 ZNAP_REPO="${XDG_DATA_HOME:-${HOME}/.local/share}/znap/_"
 
+znap-cleanup() {
+    printf '%s\n' ~/ ~/VAST/dockersh.home/ | xargs -I _ find _ -maxdepth 3 -type d -name 'znap' -or -name 'zsh-snap' -print -exec rm -fr '{}' +
+}
+
 # Download Znap, if it's not there yet.
 [[ -r "${ZNAP_REPO}/znap.zsh" ]] ||
     git clone --depth 1 -- \
